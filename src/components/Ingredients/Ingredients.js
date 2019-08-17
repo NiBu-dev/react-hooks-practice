747d4cc1fba47323356303a52d62a0a9b8b8854a
Nick <sahas023@gmail.com>
2019-08-17 18:03:42 +0200

    Added useEffect hook in order to fetch data from server -> side effect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -7,6 +7,23 @@ import IngredientList from './IngredientList';
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
+  useEffect(() => {
+    fetch('https://ingredient-maker-hooks-up.firebaseio.com/ingredients.json').then(
+      (response) => {
+        return response.json()
+      }
+    ).then((responseData) => {
+      const loadedIngredients = [];
+      for (let key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        })
+      }
+      setUserIngredients(loadedIngredients);
+    })
+  }, []);
 
   const addIngredientHandler = ingredient => {
     fetch('https://ingredient-maker-hooks-up.firebaseio.com/ingredients.json', {
