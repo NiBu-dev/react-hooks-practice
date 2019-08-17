18f8e74c55bb429520f98f44b3db1ce606fbe437
Nick <sahas023@gmail.com>
2019-08-17 17:41:50 +0200

    Added on delete ingredient from state handler + http request on Add ingredient

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,24 +1,47 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
-import IngredientList from './IngredientList'; 
+import IngredientList from './IngredientList';
 
-const  Ingredients = () => {
+const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
+
   const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [...prevIngredients, {id: Math.random().toString(), ...ingredient}]);
+    fetch('https://ingredient-maker-hooks-up.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    }).then((response) => {
+      return response.json();
+    }).then((responseData) => {
+      setUserIngredients(prevIngredients => [...prevIngredients, { id: responseData.name, ...ingredient }]);
+
+    }) // browser function - API
+  }
+
+  const removeIngredientHandler = id => {
+    setUserIngredients(prevIngredients => {
+      let newUserIngredients = prevIngredients.filter((ingredient) => {
+        if (id === ingredient.id) {
+          return false
+        } else {
+          return true
+        }
+      })
+      return newUserIngredients;
+    })
   }
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler}/>
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
         {/* Need to add list here! */}
-        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}}/>
+        <IngredientList ingredients={userIngredients} onRemoveItem={(id) => removeIngredientHandler(id)} />
       </section>
     </div>
   );
