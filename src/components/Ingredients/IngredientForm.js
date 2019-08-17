3babc49837d7e15eaebc36128fca6ec2563855a1
Nick <sahas023@gmail.com>
2019-08-17 12:44:41 +0200

    splitted the state in order to be handled separately

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -10,6 +10,10 @@ const IngredientForm = React.memo(props => {
   const submitHandler = event => {
     event.preventDefault();
     // ...
+    props.onAddIngredient({
+      title: enteredTitle,
+      amount: enteredAmount
+    })
   };
 
   return (
